477a43d87dd78a4de4a386c60f6fdb548c3f106f
Stephane <stephane.therrien@parker.com>
2018-10-29 12:46:33 -0400

    initial commit

---
+++ src/repository/user.repository.ts
@@ -0,0 +1,22 @@
+import { IUser } from '../interface/user.interface';
+
+export interface IUserRepository {
+    getAll(): Promise<Array<IUser>>;
+}
+
+export class UserRepository implements IUserRepository {
+    private _userList: Array<IUser>;
+
+    constructor() {
+        this._userList = [
+            { firstName: 'Bob', lastName: 'Marley', age: 43 },
+            { firstName: 'Patrick', lastName: 'Roy', age: 44 },
+        ];
+    }
+
+    async getAll(): Promise<Array<IUser>> {
+        let users = await this._userList;
+
+        return users;
+    }
+}
---
+++ src/service/user.service.ts
@@ -0,0 +1,18 @@
+import { IUserRepository } from '../repository/user.repository';
+import { IUser } from '../interface/user.interface';
+
+export interface IUserService{
+    getAll(): Promise<Array<IUser>>;
+}
+
+export class UserService implements IUserService{
+    private readonly _userRepository: IUserRepository;
+
+    constructor(userRepository: IUserRepository){
+        this._userRepository = userRepository;
+    }
+
+    getAll(): Promise<Array<IUser>>{
+        return this._userRepository.getAll();
+    }
+}
